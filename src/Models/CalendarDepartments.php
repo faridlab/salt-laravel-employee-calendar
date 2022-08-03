d099b4a27d30bb86b9ea8c52bccda41486ae274d
Farid Hidayat <e.faridhidayat@gmail.com>
2022-08-03 22:50:38 +0700

    feat: model CalendarDepartments

---
+++ src/Models/CalendarDepartments.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace SaltEmployeeCalendar\Models;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Database\Eloquent\Model;
+use DB;
+use Illuminate\Support\Facades\Schema;
+
+use SaltLaravel\Models\Resources;
+use SaltLaravel\Traits\ObservableModel;
+use SaltLaravel\Traits\Uuids;
+
+class CalendarDepartments extends Resources {
+
+    use Uuids;
+    use ObservableModel;
+
+    protected $filters = [
+        'default',
+        'search',
+        'fields',
+        'relationship',
+        'withtrashed',
+        'orderby',
+
+        // Fields table
+        'id',
+        'calendar_id',
+        'department_id'
+    ];
+
+    protected $rules = array(
+        'calendar_id' => 'required|string',
+        'department_id' => 'required|string',
+    );
+
+    protected $auths = array (
+        'index',
+        'store',
+        'show',
+        'update',
+        'patch',
+        'destroy',
+        'trash',
+        'trashed',
+        'restore',
+        'delete',
+        'import',
+        'export',
+        'report'
+    );
+
+    protected $structures = array();
+    protected $forms = array();
+
+    protected $searchable = array(
+        'calendar_id',
+        'department_id'
+    );
+
+    protected $fillable = array(
+        'calendar_id',
+        'department_id'
+    );
+
+}
