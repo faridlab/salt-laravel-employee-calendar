10572f2a387ae1cad36aec3bd1c691ece617eafd
Farid Hidayat <e.faridhidayat@gmail.com>
2022-08-03 22:47:03 +0700

    feat: calendar_levels migration file

---
+++ database/migrations/2022_08_03_151006_create_calendar_levels_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('calendar_levels', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+
+            $table->foreignUuid('calendar_id')->references('id')->on('calendars');
+            $table->foreignUuid('level_id')->references('id')->on('organization_structures');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('calendar_levels');
+    }
+};
