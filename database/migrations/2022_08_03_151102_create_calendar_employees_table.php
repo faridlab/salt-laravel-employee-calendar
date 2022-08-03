f33034eae213a6db5b0b84995faced77cf93c780
Farid Hidayat <e.faridhidayat@gmail.com>
2022-08-03 22:48:31 +0700

    feat: calendar_employees migration file

---
+++ database/migrations/2022_08_03_151102_create_calendar_employees_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('calendar_employees', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+
+            $table->foreignUuid('calendar_id')->references('id')->on('calendars');
+            $table->foreignUuid('employee_id')->references('id')->on('employees');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('calendar_employees');
+    }
+};
